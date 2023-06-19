e4afaa546fda5ce6470d0915cbb5ee12933b0383
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-06-19 18:25:40 +0300

    Changed pin_init function

--- Src/main.cpp
+++ Src/main.cpp
@@ -5,46 +5,47 @@
 #endif
 
 // System includes
-#include <stm32f4xx.h>
-#include <stm32f410rx.h>
 
 // User includes
-#include <nucleo_pins.h>
+#include <user_types.h>
+#include <user_functions.h>
 
-void clock_init()
-{
-  // Clock enable PORT A, B, C
-  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN);
-  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN);
-  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOCEN);
-}
+// Define all used gpio pins
+pin_struct_TypeDef LED0;
+pin_struct_TypeDef LED1;
+pin_struct_TypeDef LED2;
+pin_struct_TypeDef LED3;
+pin_struct_TypeDef LED4;
+pin_struct_TypeDef LED5;
+pin_struct_TypeDef LED6;
+pin_struct_TypeDef LED7;
+pin_struct_TypeDef LED8;
+pin_struct_TypeDef BTN0;
 
 void pin_init()
 {
-  //Set LED pins to general purpose output mode
-  LED0_SET_PIN_MODE_OUTPUT;
-  LED1_SET_PIN_MODE_OUTPUT;
-  LED2_SET_PIN_MODE_OUTPUT;
-  LED3_SET_PIN_MODE_OUTPUT;
-  LED4_SET_PIN_MODE_OUTPUT;
-  LED5_SET_PIN_MODE_OUTPUT;
-  LED6_SET_PIN_MODE_OUTPUT;
-  LED7_SET_PIN_MODE_OUTPUT;
-  LED8_SET_PIN_MODE_OUTPUT;
-  //Set BUTTON pin to input mode (reset state)
-  BUTTON0_SET_PIN_MODE_INPUT;
+  // Set LED pins to general purpose output mode
+  LED0 = pin_setup(GPIOC, PIN9, OUTPUT);
+  LED1 = pin_setup(GPIOC, PIN8, OUTPUT);
+  LED2 = pin_setup(GPIOB, PIN8, OUTPUT);
+  LED3 = pin_setup(GPIOC, PIN6, OUTPUT);
+  LED4 = pin_setup(GPIOB, PIN9, OUTPUT);
+  LED5 = pin_setup(GPIOA, PIN5, OUTPUT);
+  LED6 = pin_setup(GPIOA, PIN12, OUTPUT);
+  LED7 = pin_setup(GPIOA, PIN6, OUTPUT);
+  LED8 = pin_setup(GPIOA, PIN11, OUTPUT);
+  BTN0 = pin_setup(GPIOB, PIN2, INPUT);
+  // Set BUTTON pin to input mode (reset state)
 }
-
-
 
 int main(void)
 {
+
   clock_init();
   pin_init();
-
+  
   /* Loop forever */
-  do{
-    
-
-  }while(SET);
+  do
+  {
+  } while (SET);
 }
