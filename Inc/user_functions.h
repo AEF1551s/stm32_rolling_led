5d445371030019492e7c88fc0aa371390ba0df7d
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-06-22 20:20:30 +0300

    Added in/decrament led array functions

--- Inc/user_functions.h
+++ Inc/user_functions.h
@@ -60,4 +60,38 @@ void digital_write(pin_struct_TypeDef pin, pin_state_TypeDef mode)
   WRITE_REG(pin.GPIOx->BSRR, reset_msk); // reset pin
 }
 
+void led_array_increment(pin_struct_TypeDef LED_pins[9], int starting_position, int &delay)
+{
+
+  int i = starting_position;
+
+  digital_write(LED_pins[i], HIGH);
+  delay_ms(delay);
+  digital_write(LED_pins[i], LOW);
+
+  i++;
+
+  if (i == 9)
+    i = 0;
+
+  led_array_increment(LED_pins, i, delay);
+}
+
+void led_array_decrement(pin_struct_TypeDef LED_pins[9], int starting_position, int &delay)
+{
+
+  int i = starting_position;
+
+  digital_write(LED_pins[i], HIGH);
+  delay_ms(delay);
+  digital_write(LED_pins[i], LOW);
+
+  i--;
+
+  if (i == -1)
+    i = 8;
+
+  led_array_decrement(LED_pins, i, delay);
+}
+
 #endif // USER_FUNCTIONS_H
